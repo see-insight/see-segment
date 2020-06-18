06dee4c1719ab1fe3961d2e6aad93c9f0969e18e
Kai <kai.pinckard@gmail.com>
2020-06-17 19:43:26 -0700

    Fix unreadable images by appending ?raw=true to wget command

--- container.py
+++ container.py
@@ -1,42 +1,45 @@
-import imageio
-import numpy as np
-from see import GeneticSearch, Segmentors
-import os
+if __name__ == "__main__":
+    import imageio
+    import numpy as np
+    from see import GeneticSearch, Segmentors
+    import os
 
-POP_SIZE = 10
-NUM_GENERATIONS = 5
+    # 
+    POP_SIZE = 10
+    NUM_GENERATIONS = 5
+    INPUT_IMAGE = "input.jpg"
+    LABEL_IMAGE = "label.png"
 
-rgb_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon.jpg"
-label_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon_GT.png"
+    rgb_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon.jpg"
+    label_url = "https://github.com/colbrydi/see-segment/blob/master/Image_data/Examples/Chameleon_GT.png"
 
-def make_download_command(url, save_as):
-    """
-    Used to construct a curl command to download
-    the image at the specified url.
-    """
-    return "wget -O " + str(save_as) + " " + str(url) + "?raw=true"
+    def make_download_command(url, save_as):
+        """
+        Used to construct a curl command to download
+        the image at the specified url.
+        """
+        return "wget -O " + str(save_as) + " " + str(url) + "?raw=true"
 
-# Download the images
-os.system(make_download_command(rgb_url, "rgb.jpg"))
-os.system(make_download_command(label_url, "label.png"))
+    # Download the images
+    os.system(make_download_command(rgb_url, INPUT_IMAGE))
+    os.system(make_download_command(label_url, LABEL_IMAGE))
 
-print(make_download_command(rgb_url, "rgb.jpg"))
-# Load the images
-img = imageio.imread('rgb.jpg')
-gmask = imageio.imread('label.png')
+    # Load the images
+    img = imageio.imread(INPUT_IMAGE)
+    gmask = imageio.imread(LABEL_IMAGE)
 
-#only need on some images
-# Convert the RGB 3-channel image into a 1-channel image
-#gmask = (np.sum(gmask, axis=2) > 0)
+    #only need on some images
+    # Convert the RGB 3-channel image into a 1-channel image
+    #gmask = (np.sum(gmask, axis=2) > 0)
 
-# Conduct the genetic search
-my_evolver = GeneticSearch.Evolver(img, gmask, pop_size=POP_SIZE)
-population = my_evolver.run(ngen=NUM_GENERATIONS)
+    # Conduct the genetic search
+    my_evolver = GeneticSearch.Evolver(img, gmask, pop_size=POP_SIZE)
+    population = my_evolver.run(ngen=NUM_GENERATIONS)
 
-# Take the best segmentor from the hof and use it to segment the rgb image
-seg = Segmentors.algoFromParams(my_evolver.hof[0])
-mask = seg.evaluate(img)
+    # Take the best segmentor from the hof and use it to segment the rgb image
+    seg = Segmentors.algoFromParams(my_evolver.hof[0])
+    mask = seg.evaluate(img)
 
-# Calculate and print the fitness value of the segmentor
-fitness = Segmentors.FitnessFunction(mask, gmask)[0]
-print(fitness)
+    # Calculate and print the fitness value of the segmentor
+    fitness = Segmentors.FitnessFunction(mask, gmask)[0]
+    print("fitness:", fitness)
