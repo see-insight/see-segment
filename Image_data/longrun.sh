00f7a03d9597bf03c87c8af769bd53a74f4b2bb6
Dirk Colbry <colbyrdi@msu.edu>
2020-06-20 16:39:31 -0400

    Working on WACV figures data

--- Image_data/longrun.sh
+++ Image_data/longrun.sh
@@ -2,9 +2,20 @@
 
 rm STOP.STOP
 
-echo "Starting Timer"
-export STOP_TIME=$(( 6 * 60 * 60 ))
+export HOURS=2
 
-sleep $STOP_TIME 
+for hour in  $(seq 1 $HOURS)
+do
+    echo "Starting $hour of $HOURS hours"
+    sleep $(( 60 * 60 )) #Hours
+    echo "Completed $hour of $HOURS hours"
+    date
+    if [ -f STOP.STOP ]
+    then
+        echo "STOP.STOP file Exists.  Stoping timer" 
+        echo 1
+    fi
+done
+
 echo "STOPPING"
 touch STOP.STOP
