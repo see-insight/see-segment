fd4b454c650d654ecee4fc07993eb981f4ddfddf
lindavin <lindavin@grinnell.edu>
2021-06-01 15:16:59 -0400

    Add two new classifiers

--- see/classifiers.py
+++ see/classifiers.py
@@ -5,8 +5,11 @@
 import numpy as np
 #import sklearn
 
+from sklearn.tree import DecisionTreeClassifier as DecisionTree
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier as kNearestNeighbors
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 from see.base_classes import param_space, algorithm
 
@@ -22,12 +25,17 @@ ClassifierParams.add('algorithm',
                      [],
                      "string code for the algorithm")
 
+ClassifierParams.add('h',
+                     [float(i) / 256 for i in range(0, 256)],
+                     "step size in the mesh"
+                     )
+
 
 class Classifier(algorithm):
     """Base class for classifier classes defined below.
 
     Functions:
-    evaluate -- Run classifier algorithm to...
+    evaluate -- Run classifier algorithm to classify dataset.
 
     """
 
@@ -39,11 +47,12 @@ class Classifier(algorithm):
         self.params = ClassifierParams()
         self.set_params(paramlist)
 
+    # Input: labelled dataset
+    # Output: Tuple with prediction probabilities, score
     def evaluate(self, dataset):
         """Instance evaluate method. Needs to be overridden by subclasses."""
-        print("Default do nothing evaluate method")
-        print(self.params)
-        print(dataset)
+        # Default method does nothing and returns score 0 with not prediction
+        return [dataset, 0]
 
     @classmethod
     def add_classifier(cls, key, classifier):
@@ -52,25 +61,34 @@ class Classifier(algorithm):
         cls.algorithmspace[key] = classifier
 
 
-class NaiveBayes(Classifier):
+class GaussianNBClassifier(Classifier):
     """Perform Gaussian Naive Bayes classification algorithm."""
 
     def __init__(self, paramlist=None):
         """Gaussian Naive Bayes has no parameters in the example."""
         super().__init__()
+        self.params = ClassifierParams()
+
+        self.params["h"] = .02
         self.set_params(paramlist)
 
     def evaluate(self, dataset):
         """The evaluate function for Gaussian Naive Bayes"""
 
-        h = .02  # step size in the mesh
+        h = self.params["h"]  # step size in the mesh
 
         clf = GaussianNB()
         X, y = dataset
         X = StandardScaler().fit_transform(X)
 
-        clf.fit(X, y)
+        # TODO: test_size and random_state are hard-coded
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=.4, random_state=42)
 
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        # TODO: Generating the mesh grid can be a Class method
         x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
         y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
         xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -79,7 +97,91 @@ class NaiveBayes(Classifier):
         Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
         Z = Z.reshape(xx.shape)
 
-        return Z
+        return [Z, score]
 
 
-Classifier.add_classifier('Naive Bayes', NaiveBayes)
+Classifier.add_classifier('Gaussian Naive Bayes', GaussianNBClassifier)
+
+
+class KNeighborsClassifier(Classifier):
+    """Perform K Nearest-Neighbors classification algorithm."""
+
+    def __init__(self, paramlist=None):
+        super().__init__()
+        self.params = ClassifierParams()
+
+        self.params["h"] = .02
+        self.params["k"] = 3
+        self.set_params(paramlist)
+
+    def evaluate(self, dataset):
+        """The evaluate function for K Nearest-Neighbors"""
+
+        h = self.params["h"]  # step size in the mesh
+
+        clf = kNearestNeighbors(self.params["k"])
+        X, y = dataset
+        X = StandardScaler().fit_transform(X)
+
+        # TODO: test_size and random_state are hard-coded
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=.4, random_state=42)
+
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        # TODO: Generating the mesh grid can be a Class method
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        Z = Z.reshape(xx.shape)
+
+        return [Z, score]
+
+
+Classifier.add_classifier('K Nearest Neighbors', KNeighborsClassifier)
+
+
+class DecisionTreeClassifier(Classifier):
+    """Perform Decision Tree classification algorithm."""
+
+    def __init__(self, paramlist=None):
+        super().__init__()
+        self.params = ClassifierParams()
+
+        self.params["h"] = .02
+        self.params["max_depth"] = 5
+        self.set_params(paramlist)
+
+    def evaluate(self, dataset):
+        """The evaluate function for K Nearest-Neighbors"""
+
+        h = self.params["h"]  # step size in the mesh
+
+        clf = DecisionTree(max_depth=self.params["max_depth"])
+        X, y = dataset
+        X = StandardScaler().fit_transform(X)
+
+        # TODO: test_size and random_state are hard-coded
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=.4, random_state=42)
+
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        # TODO: Generating the mesh grid can be a Class method
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        Z = Z.reshape(xx.shape)
+
+        return [Z, score]
+
+
+Classifier.add_classifier('Decision Tree', DecisionTreeClassifier)
--- see/tests/test_classifiers.py
+++ see/tests/test_classifiers.py
@@ -2,15 +2,20 @@
 
 import numpy as np
 
-from see import classifiers
-from sklearn.datasets import make_moons #, make_circles, make_classification
+from sklearn.datasets import make_moons  # , make_circles, make_classification
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier as DecisionTree
+from sklearn.model_selection import train_test_split
+
+from see import classifiers
 
 
 def test_gaussian_naive_bayes():
-    """Unit test for GNB. Check if evaluate function output is same as
-    it would be running the sklearn function manually."""
+    """Unit test for Gaussian Naive Bayes classifer algorithm.
+    Check if evaluate function output is same as it would be
+    running the sklearn function manually."""
 
     # Generate dataset
     h = 0.02
@@ -19,6 +24,9 @@ def test_gaussian_naive_bayes():
     X, y = dataset
     X = StandardScaler().fit_transform(X)
 
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.4, random_state=42)
+
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -26,16 +34,102 @@ def test_gaussian_naive_bayes():
 
     clf = GaussianNB()
 
-    clf.fit(X, y)  # Train classifier
+    clf.fit(X_train, y_train)  # Train classifier
 
-    # sklearn categorizations
+    # manual sklearn categorizations
     expected_predictions = clf.predict_proba(
         np.c_[xx.ravel(), yy.ravel()])[:, 1]
     expected_predictions = expected_predictions.reshape(xx.shape)
 
+    # manual sklearn fitness score
+    expected_score = clf.score(X_test, y_test)
+
     # see-classify categorizations
-    see_clf = classifiers.NaiveBayes()
-    actual_predictions = see_clf.evaluate(dataset)
+    see_clf = classifiers.GaussianNBClassifier()
+    [actual_predictions, actual_score] = see_clf.evaluate(dataset)
 
     assert len(actual_predictions) == len(expected_predictions)
     assert (actual_predictions == expected_predictions).all()
+    assert expected_score == expected_score
+
+
+def test_nearest_neighbor():
+    """Unit test for Nearest Neighbors classifier algorithm.
+    Check if evaluate function output is same as it would be
+    running the sklearn function manually."""
+
+    # Generate dataset
+    h = 0.02
+
+    dataset = make_moons(noise=0.3, random_state=0)
+    X, y = dataset
+    X = StandardScaler().fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.4, random_state=42)
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    clf = KNeighborsClassifier(3)
+
+    clf.fit(X_train, y_train)  # Train classifier
+
+    # manual sklearn categorizations
+    expected_predictions = clf.predict_proba(
+        np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    expected_predictions = expected_predictions.reshape(xx.shape)
+
+    # manual sklearn fitness score
+    expected_score = clf.score(X_test, y_test)
+
+    # see-classify categorizations
+    see_clf = classifiers.KNeighborsClassifier()
+    [actual_predictions, actual_score] = see_clf.evaluate(dataset)
+
+    assert len(actual_predictions) == len(expected_predictions)
+    assert (actual_predictions == expected_predictions).all()
+    assert expected_score == expected_score
+
+
+def test_decision_tree():
+    """Unit test for Decision Tree classifier algorithm.
+    Check if evaluate function output is same as it would be
+    running the sklearn function manually."""
+
+    # Generate dataset
+    h = 0.02
+
+    dataset = make_moons(noise=0.3, random_state=0)
+    X, y = dataset
+    X = StandardScaler().fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.4, random_state=42)
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    clf = DecisionTree(max_depth=5)
+
+    clf.fit(X_train, y_train)  # Train classifier
+
+    # manual sklearn categorizations
+    expected_predictions = clf.predict_proba(
+        np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    expected_predictions = expected_predictions.reshape(xx.shape)
+
+    # manual sklearn fitness score
+    expected_score = clf.score(X_test, y_test)
+
+    # see-classify categorizations
+    see_clf = classifiers.DecisionTreeClassifier()
+    [actual_predictions, actual_score] = see_clf.evaluate(dataset)
+
+    assert len(actual_predictions) == len(expected_predictions)
+    assert (actual_predictions == expected_predictions).all()
+    assert expected_score == expected_score
