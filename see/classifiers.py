76784a4e1ec17c384b550bc89016d52d128bf0d4
lindavin <lindavin@grinnell.edu>
2021-06-25 10:34:42 -0500

    Format code and fix AdaBoost

--- see/classifiers.py
+++ see/classifiers.py
@@ -5,61 +5,68 @@
   Genetic Search algorithm."""
 
 import numpy as np
-#import sklearn
+
+# import sklearn
 
 from sklearn.tree import DecisionTreeClassifier as DecisionTree
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier as kNearestNeighbors
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    ExtraTreesClassifier,
+    GradientBoostingClassifier,
+    AdaBoostClassifier,
+)
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 
 from see.base_classes import param_space, algorithm, pipedata
 
+
 class ClassifierParams(param_space):
     """Parameter space for classifiers
     """
+
     descriptions = dict()
     ranges = dict()
 
 
-ClassifierParams.add('algorithm',
-                     [],
-                     "string code for the algorithm")
+ClassifierParams.add("algorithm", [], "string code for the algorithm")
 
 # TODO: Many of these parameters' ranges were set arbitrarily.
 # The max_param were especially arbitrary as some of the documentation
 # do not give a suggested range.
 
-ClassifierParams.add("max_iter",
-                     [i for i in range(1, 101)],
-                     'Number of iterations for the algorithm')
+ClassifierParams.add(
+    "max_iter", [i for i in range(1, 101)], "Number of iterations for the algorithm"
+)
 
-ClassifierParams.add("alpha",
-                     [float(i)/1000 for i in range(1, 1000)],
-                     'regularization parameter')
+ClassifierParams.add(
+    "alpha", [float(i) / 1000 for i in range(1, 1000)], "regularization parameter"
+)
 
-ClassifierParams.add("max_depth",
-                     [i for i in range(1, 10)],
-                     'Maximum depth of tree')
+ClassifierParams.add("max_depth", [i for i in range(1, 10)], "Maximum depth of tree")
 
-ClassifierParams.add("n_estimators",
-                     [i for i in range(1, 100)],
-                     'Number of trees in the forest')
+ClassifierParams.add(
+    "n_estimators", [i for i in range(1, 100)], "Number of trees in the forest"
+)
 
-ClassifierParams.add("n_neighbors",
-                     [i for i in range(1, 10)],
-                     'Number of neighbors to use')
+ClassifierParams.add(
+    "n_neighbors", [i for i in range(1, 10)], "Number of neighbors to use"
+)
 
-ClassifierParams.add("length_scale",
-                     [float(i)/10 for i in range(1, 10)],
-                     'The length scale of the kernel.')
+ClassifierParams.add(
+    "length_scale",
+    [float(i) / 10 for i in range(1, 10)],
+    "The length scale of the kernel.",
+)
 
-ClassifierParams.add("learning_rate",
-                     [float(i)/10 for i in range(1, 10)],
-                     'The learning rate')
+ClassifierParams.add(
+    "learning_rate", [float(i) / 10 for i in range(1, 10)], "The learning rate"
+)
+
 
 class Classifier(algorithm):
     """Base class for classifier classes defined below.
@@ -70,6 +77,8 @@ class Classifier(algorithm):
     """
 
     algorithmspace = dict()
+    # TODO: All Classifiers need the paramindexes field; cannot default to []
+    # in order to print properly
 
     def __init__(self, paramlist=None, paramindexes=[]):
         """Generate algorithm params from parameter list."""
@@ -91,14 +100,14 @@ class Classifier(algorithm):
     # Output: Tuple with prediction probabilities, score
     def evaluate(self, training_set, testing_set):
         """Instance evaluate method. Needs to be overridden by subclasses."""
-        self.thisalgo = Classifier.algorithmspace[self.params['algorithm']](self.params)
+        self.thisalgo = Classifier.algorithmspace[self.params["algorithm"]](self.params)
         return self.thisalgo.evaluate(training_set, testing_set)
 
     def pipe(self, data):
         print(data)
-        self.thisalgo = Classifier.algorithmspace[self.params['algorithm']](self.params)
+        self.thisalgo = Classifier.algorithmspace[self.params["algorithm"]](self.params)
         is_data_k_folds = data.k_folds
-        if(is_data_k_folds):
+        if is_data_k_folds:
             training_folds = data.training_folds
             testing_folds = data.testing_folds
             data.predictions = list(map(self.evaluate, training_folds, testing_folds))
@@ -109,7 +118,7 @@ class Classifier(algorithm):
     @classmethod
     def add_classifier(cls, key, classifier):
         """Adds the classifier to the algorithm space."""
-        ClassifierParams.ranges['algorithm'].append(key)
+        ClassifierParams.ranges["algorithm"].append(key)
         cls.algorithmspace[key] = classifier
 
 
@@ -118,7 +127,7 @@ class GaussianNBClassifier(Classifier):
 
     def __init__(self, paramlist=None):
         """Gaussian Naive Bayes has no parameters in the example."""
-        
+
         super(GaussianNBClassifier, self).__init__(paramlist)
 
         self.params["algorithm"] = "Gaussian Naive Bayes"
@@ -132,7 +141,7 @@ class GaussianNBClassifier(Classifier):
         return clf.predict(testing_set.X)
 
 
-Classifier.add_classifier('Gaussian Naive Bayes', GaussianNBClassifier)
+Classifier.add_classifier("Gaussian Naive Bayes", GaussianNBClassifier)
 
 
 class KNeighborsClassifier(Classifier):
@@ -140,7 +149,7 @@ class KNeighborsClassifier(Classifier):
 
     def __init__(self, paramlist=None):
         super().__init__()
-        
+
         self.params["algorithm"] = "K Nearest Neighbors"
         self.params["n_neighbors"] = 3
         self.set_params(paramlist)
@@ -154,12 +163,18 @@ class KNeighborsClassifier(Classifier):
         # Modulo might not be the best way to do this.
         neighbors_param = self.params["n_neighbors"]
         param_in_range = (neighbors_param <= num_samples) and (neighbors_param > 0)
-        clf = kNearestNeighbors(n_neighbors=(neighbors_param if param_in_range else (neighbors_param % num_samples + 1)))
+        clf = kNearestNeighbors(
+            n_neighbors=(
+                neighbors_param
+                if param_in_range
+                else (neighbors_param % num_samples + 1)
+            )
+        )
         clf.fit(training_set.X, training_set.y)
         return clf.predict(testing_set.X)
 
 
-Classifier.add_classifier('K Nearest Neighbors', KNeighborsClassifier)
+Classifier.add_classifier("K Nearest Neighbors", KNeighborsClassifier)
 
 
 class DecisionTreeClassifier(Classifier):
@@ -180,7 +195,7 @@ class DecisionTreeClassifier(Classifier):
         return clf.predict(testing_set.X)
 
 
-Classifier.add_classifier('Decision Tree', DecisionTreeClassifier)
+Classifier.add_classifier("Decision Tree", DecisionTreeClassifier)
 
 
 class RandomForestContainer(Classifier):
@@ -196,16 +211,17 @@ class RandomForestContainer(Classifier):
 
     def evaluate(self, training_set, testing_set):
         """The evaluate function for Decision Trees."""
-        
+
         clf = RandomForestClassifier(
-            max_depth=self.params["max_depth"],
-            n_estimators=self.params["n_estimators"])
+            max_depth=self.params["max_depth"], n_estimators=self.params["n_estimators"]
+        )
 
         clf.fit(training_set.X, training_set.y)
         return clf.predict(testing_set.X)
 
 
-Classifier.add_classifier('Random Forest', RandomForestContainer)
+Classifier.add_classifier("Random Forest", RandomForestContainer)
+
 
 class MLPContainer(Classifier):
     """Perform MLP Neural Network classification algorithm."""
@@ -228,7 +244,8 @@ class MLPContainer(Classifier):
         return clf.predict(testing_set.X)
 
 
-Classifier.add_classifier('MLP Neural Network', MLPContainer)
+Classifier.add_classifier("MLP Neural Network", MLPContainer)
+
 
 class GaussianProcessContainer(Classifier):
     """Perform Guassian Process classification algorithm."""
@@ -249,7 +266,9 @@ class GaussianProcessContainer(Classifier):
 
         return clf.predict(testing_set.X)
 
-Classifier.add_classifier('Gaussian Process', GaussianProcessContainer)
+
+Classifier.add_classifier("Gaussian Process", GaussianProcessContainer)
+
 
 class ExtraTreesContainer(Classifier):
     """Perform Guassian Process classification algorithm."""
@@ -265,13 +284,17 @@ class ExtraTreesContainer(Classifier):
     def evaluate(self, training_set, testing_set):
         """The evaluate function for Extra Trees."""
 
-        clf = ExtraTreesClassifier(n_estimators=self.params["n_estimators"], max_depth=self.params["max_depth"])
+        clf = ExtraTreesClassifier(
+            n_estimators=self.params["n_estimators"], max_depth=self.params["max_depth"]
+        )
 
         clf.fit(training_set.X, training_set.y)
 
         return clf.predict(testing_set.X)
 
-Classifier.add_classifier('Extra Trees', ExtraTreesContainer)
+
+Classifier.add_classifier("Extra Trees", ExtraTreesContainer)
+
 
 class GradientBoostingContainer(Classifier):
     """Perform Gradient Boosting classification algorithm."""
@@ -287,13 +310,18 @@ class GradientBoostingContainer(Classifier):
     def evaluate(self, training_set, testing_set):
         """The evaluate function for Gradient Boosting."""
 
-        clf = GradientBoostingClassifier(n_estimators=self.params["n_estimators"], learning_rate=self.params["learning_rate"])
+        clf = GradientBoostingClassifier(
+            n_estimators=self.params["n_estimators"],
+            learning_rate=self.params["learning_rate"],
+        )
 
         clf.fit(training_set.X, training_set.y)
 
         return clf.predict(testing_set.X)
 
-Classifier.add_classifier('Gradient Boosting', GradientBoostingContainer)
+
+Classifier.add_classifier("Gradient Boosting", GradientBoostingContainer)
+
 
 class AdaBoostContainer(Classifier):
     """Perform Gradient Boosting classification algorithm."""
@@ -309,10 +337,14 @@ class AdaBoostContainer(Classifier):
     def evaluate(self, training_set, testing_set):
         """The evaluate function for Ada Boost."""
 
-        clf = GradientBoostingClassifier(n_estimators=self.params["n_estimators"], learning_rate=self.params["learning_rate"])
+        clf = AdaBoostClassifier(
+            n_estimators=self.params["n_estimators"],
+            learning_rate=self.params["learning_rate"],
+        )
 
         clf.fit(training_set.X, training_set.y)
 
         return clf.predict(testing_set.X)
 
-Classifier.add_classifier('Ada Boost', AdaBoostContainer)
+
+Classifier.add_classifier("Ada Boost", AdaBoostContainer)
