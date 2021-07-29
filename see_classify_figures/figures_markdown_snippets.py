cddd124be7c7c0e320354f4be9ef6ea805fd8811
lindavin <lindavin@grinnell.edu>
2021-07-29 07:16:33 -0500

    Write instructions using snippet

---
+++ see_classify_figures/figures_markdown_snippets.py
@@ -0,0 +1,36 @@
+from IPython.display import display, Markdown
+
+data_creation_instructions = """
+To generate data, run:
+- for sklearn
+    ```bash
+sbatch generate_sklearn_data.sb -n 10 -p 10
+    ```
+- for dhahri
+    ```bash
+sbatch generate_dhahri_data.sb -n 20 -p 100 -t 30
+    ```
+These commands generate files for running genetic search using 10 generations and population size of 10
+for the sklearn tutorial and 20 generations with population size of 100
+for the Breast Cancer Wisconsin (Diagnostic) Dataset respectively.
+
+The `-n`, `-p`, `-t` flags control number of generations, population sizes, 
+and number of trials respectively.
+
+The data generated tracks the top 10 individuals and the population by
+each generation and is stored in the corresponding output 
+(i.e. the slurm_\[id\].out file).
+
+To extract this data to a csv file, run:
+- for the top 10 individuals:
+    ```bash
+grep "# GEN HOF_index" slurm_[id].out | cut -d '|' -f2 > "filename_1.csv"
+    ```
+- for the population:
+    ```bash
+grep "# GEN population_index" slurm_[id].out | cut -d '|' -f2 > "filename_2.csv"
+    ```
+"""
+
+def show_data_instructions():
+    display(Markdown(data_creation_instructions))
