006aa269f6181a93922ccce467344487806c0560
lindavin <lindavin@grinnell.edu>
2021-08-04 11:12:14 -0500

    Fix and reduce unit tests

--- see/tests/test_classifiers.py
+++ see/tests/test_classifiers.py
@@ -2,211 +2,107 @@
 
 import numpy as np
 
-from sklearn.datasets import make_moons  # , make_circles, make_classification
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier as DecisionTree
 
-from sklearn.model_selection import train_test_split
+from see.classifier_helpers.fetch_data import generate_tutorial_data
+from see.classifier_helpers.helpers import generate_pipeline_dataset
+
 from see.base_classes import pipedata
 
 from see import classifiers
 
 
-def test_gaussian_naive_bayes():
+def test_gaussian_naive_bayes_defaults():
     """Unit test for Gaussian Naive Bayes classifer algorithm.
-    Check if evaluate function output is same as it would be
-    running the sklearn function manually."""
+    Check if classifier container with default parameters 
+    performs the same as running the corresponding sklearn algorithm
+    with their default parameters."""
 
     # Generate dataset
-    dataset = make_moons(noise=0.3, random_state=0)
-    X, y = dataset
-    X = StandardScaler().fit_transform(X)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=.4, random_state=42)
+    datasets = generate_tutorial_data()
 
     clf = GaussianNB()
 
-    clf.fit(X_train, y_train)  # Train classifier
-
     # manual sklearn categorizations
-    expected_predictions = clf.predict(X_test)
+    expected_predictions = []
+    for ds_name in datasets:
+        X, y = datasets[ds_name]
+        clf.fit(X, y)  # Train classifier
+        expected_predictions.append(clf.predict(X))
 
-    # see-classify categorizations
-    see_clf = classifiers.GaussianNBClassifier()
-    testing_set = pipedata()
-    testing_set.X = X_test
-    testing_set.y = y_test
-
-    training_set = pipedata()
-    training_set.X = X_train
-    training_set.y = y_train
+    clf_container = classifiers.GaussianNBContainer()
     
-    actual_predictions = see_clf.evaluate(training_set, testing_set)
+    # Check that default params are equal
+    assert (clf_container.create_clf().get_params() == clf.get_params())
+    
+    # Check that the evaluate function works correctly
+    for i, ds_name in enumerate(datasets):
+        X, y = datasets[ds_name]
+        pipeline_ds = generate_pipeline_dataset(X, y)
+        actual_predictions = clf_container.evaluate(pipeline_ds.training_set, pipeline_ds.testing_set)
+        assert len(actual_predictions) == len(expected_predictions[i])
+        assert (actual_predictions == expected_predictions[i]).all()
 
-    assert len(actual_predictions) == len(expected_predictions)
-    assert (actual_predictions == expected_predictions).all()
-
-def test_nearest_neighbor():
-    """Unit test for Nearest Neighbors classifier algorithm.
-    Check if evaluate function output is same as it would be
-    running the sklearn function manually."""
+def test_nearest_neighbor_defaults():
+    """Unit test for Nearest Neighbors classifer algorithm.
+    Check if classifier container with default parameters 
+    performs the same as running the corresponding sklearn algorithm
+    with their default parameters."""
 
     # Generate dataset
-    dataset = make_moons(noise=0.3, random_state=0)
-    X, y = dataset
-    X = StandardScaler().fit_transform(X)
+    datasets = generate_tutorial_data()
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=.4, random_state=42)
-
-    n_neighbors = 3
-    clf = KNeighborsClassifier(n_neighbors)
-
-    
-    clf.fit(X_train, y_train)  # Train classifier
+    clf = KNeighborsClassifier()
 
     # manual sklearn categorizations
-    expected_predictions = clf.predict(X_test)
+    expected_predictions = []
+    for ds_name in datasets:
+        X, y = datasets[ds_name]
+        clf.fit(X, y)  # Train classifier
+        expected_predictions.append(clf.predict(X))
 
-    # see-classify categorizations
-    testing_set = pipedata()
-    testing_set.X = X_test
-    testing_set.y = y_test
-
-    training_set = pipedata()
-    training_set.X = X_train
-    training_set.y = y_train
-        
-    see_clf = classifiers.KNeighborsClassifier()
-    actual_predictions = see_clf.evaluate(training_set, testing_set)
-
-    assert see_clf.params["n_neighbors"] == n_neighbors
-    assert len(actual_predictions) == len(expected_predictions)
-    assert (actual_predictions == expected_predictions).all()
+    clf_container = classifiers.KNeighborsContainer()
+    
+    # Check that default params are equal
+    assert (clf_container.create_clf().get_params() == clf.get_params())
+    
+    # Check that the evaluate function works correctly
+    for i, ds_name in enumerate(datasets):
+        X, y = datasets[ds_name]
+        pipeline_ds = generate_pipeline_dataset(X, y)
+        actual_predictions = clf_container.evaluate(pipeline_ds.training_set, pipeline_ds.testing_set)
+        assert len(actual_predictions) == len(expected_predictions[i])
+        assert (actual_predictions == expected_predictions[i]).all()
 
 def test_decision_tree():
     """Unit test for Decision Tree classifier algorithm.
-    Check if evaluate function output is same as it would be
-    running the sklearn function manually."""
+    Check if classifier container with default parameters 
+    performs the same as running the corresponding sklearn algorithm
+    with their default parameters."""
 
     # Generate dataset
-    dataset = make_moons(noise=0.3, random_state=0)
-    X, y = dataset
-    X = StandardScaler().fit_transform(X)
+    datasets = generate_tutorial_data()
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=.4, random_state=42)
-
-    clf = DecisionTree(max_depth=5)
-
-    clf.fit(X_train, y_train)  # Train classifier
+    clf = DecisionTree()
 
     # manual sklearn categorizations
-    expected_predictions = clf.predict(X_test)
+    expected_predictions = []
+    for ds_name in datasets:
+        X, y = datasets[ds_name]
+        clf.fit(X, y)  # Train classifier
+        expected_predictions.append(clf.predict(X))
 
-    # see-classify categorizations
-    testing_set = pipedata()
-    testing_set.X = X_test
-    testing_set.y = y_test
-
-    training_set = pipedata()
-    training_set.X = X_train
-    training_set.y = y_train
-
-    see_clf = classifiers.DecisionTreeClassifier()
-    actual_predictions = see_clf.evaluate(training_set, testing_set)
-
-    assert len(actual_predictions) == len(expected_predictions)
-    assert (actual_predictions == expected_predictions).all()
-
-
-
-
-def test_random_forest():
-    """Unit test for Decision Tree classifier algorithm.
-    Check if evaluate function output is same as it would be
-    running the sklearn function manually."""
-
-    # TODO: This test sometimes fails. I think it is because the algorithm
-    # has a randomness to it. Look into the random_state parameter
-    # to control this.
+    clf_container = classifiers.DecisionTreeContainer()
     
-    # Generate dataset
-    dataset = make_moons(noise=0.3, random_state=0)
-    X, y = dataset
-    X = StandardScaler().fit_transform(X)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=.4, random_state=42)
-
-
-    clf = RandomForestClassifier(max_depth=5, n_estimators=10)
-
-    clf.fit(X_train, y_train)  # Train classifier
-
-    # manual sklearn categorizations
-    expected_predictions = clf.predict(X_test)
-
-    # manual sklearn fitness score
-    testing_set = pipedata()
-    testing_set.X = X_test
-    testing_set.y = y_test
-
-    training_set = pipedata()
-    training_set.X = X_train
-    training_set.y = y_train
-
-    # see-classify categorizations
-    see_clf = classifiers.RandomForestContainer()
-    actual_predictions = see_clf.evaluate(training_set, testing_set)
-
-    assert len(actual_predictions) == len(expected_predictions)
-    assert (actual_predictions == expected_predictions).all()
-
-
-def test_mlp():
-    """Unit test for Decision Tree classifier algorithm.
-    Check if evaluate function output is same as it would be
-    running the sklearn function manually."""
-
-    # TODO: This test sometimes fails. I think it is because the algorithm
-    # has a randomness to it. Look into the random_state parameter
-    # to control this.
+    # Check that default params are equal
+    assert (clf_container.create_clf().get_params() == clf.get_params())
     
-    # Generate dataset
-    h = 0.02
-
-    dataset = make_moons(noise=0.3, random_state=0)
-    X, y = dataset
-    X = StandardScaler().fit_transform(X)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=.4, random_state=42)
-
-    clf = MLPClassifier(alpha=1, max_iter=1000)
-
-    clf.fit(X_train, y_train)  # Train classifier
-
-    # manual sklearn categorizations
-    expected_predictions = clf.predict(X_test)
-   
-    # manual sklearn fitness score
-    testing_set = pipedata()
-    testing_set.X = X_test
-    testing_set.y = y_test
-
-    training_set = pipedata()
-    training_set.X = X_train
-    training_set.y = y_train
-   
-    # see-classify categorizations
-    see_clf = classifiers.MLPContainer()
-    actual_predictions = see_clf.evaluate(training_set, testing_set)
-
-    assert len(actual_predictions) == len(expected_predictions)
-    assert (actual_predictions == expected_predictions).all()
+    # Check that the evaluate function works correctly
+    for i, ds_name in enumerate(datasets):
+        X, y = datasets[ds_name]
+        pipeline_ds = generate_pipeline_dataset(X, y)
+        actual_predictions = clf_container.evaluate(pipeline_ds.training_set, pipeline_ds.testing_set)
+        assert len(actual_predictions) == len(expected_predictions[i])
+        assert (actual_predictions == expected_predictions[i]).all()
