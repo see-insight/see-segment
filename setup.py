f686519c3fbff49e44306c6ae56a9c815d21b67c
Dirk Colbry <colbrydi@msu.edu>
2022-07-13 06:36:08 -0500

    adding imageio to list of requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires = [
+        'imageio',
         'numpy',
         'matplotlib',
         'scikit-image',
