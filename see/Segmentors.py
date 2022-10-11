560481dca3442987e9fd4b147ec9e490b0a44288
Dirk Colbry <dirk@colbry.com>
2022-10-11 14:10:10 -0400

    Missed changing one num_iter to max_num_iter

--- see/Segmentors.py
+++ see/Segmentors.py
@@ -718,7 +718,7 @@ class Morphological_Chan_Vese(segmentor):
 
         output = skimage.segmentation.morphological_chan_vese(
             img,
-            num_iter=max_num_iter,
+            max_num_iter=max_num_iter,
             init_level_set=init_level_set,
             smoothing=smoothing,
             lambda1=lambda1,
