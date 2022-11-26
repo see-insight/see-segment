565709dc916e837d7463ef225b7b3308f1a8254b
Dirk Colbry <colbyrdi@msu.edu>
2022-11-26 08:20:21 -0500

    Changing default Fitness to 1.5 instead of 1 to help debugging

--- see/Segment_Fitness.py
+++ see/Segment_Fitness.py
@@ -489,7 +489,8 @@ def FF_ML2DHD_V2(inferred, ground_truth):
     if len(test) == 1:
         # Trivial Solution
         #print(f"trivial solution")
-        error = 1
+        #ERROR - Length of test is only 1
+        error = 1.5
     else:
         error = (p / TP + np.abs(n - m) / (n + m)
                  )**(1 - np.abs(n - m) / (n + m))
